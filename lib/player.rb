be280d3d32356ace387627127924f0ce46b50505
Amadou Ibrahim <amadou140589@gmail.com>
2019-11-28 22:32:29 +0100

    Reviews errors fixed

--- lib/player.rb
+++ lib/player.rb
@@ -1,16 +1,12 @@
 # frozen_string_literal: true
 
 class Player
-  attr_reader :number, :name, :symbol
-  attr_accessor :wins
-  def initialize(number, name = nil)
+  attr_reader :number, :name, :symbol, :wins
+  attr_writer :wins
+  def initialize(number, name = nil, symbol = nil)
     @number = number
     @name = name
     @wins = 0
-    @symbol = if number == 1
-                'X'
-              else
-                'O'
-              end
+    @symbol = symbol
   end
 end
